3016e4820377b413e0100650c7a65420d5fb2fdb
Guilherme <joao.g.f@icloud.com>
2022-06-14 14:25:53 -0300

    feat: graphql setup

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,10 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from 'src/database/database.module';
-import { TestController } from './test.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'node:path';
+
+import { DatabaseModule } from '../database/database.module';
+import { TestResolver } from './test.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
-  controllers: [TestController],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [TestResolver],
 })
 export class HttpModule {}
